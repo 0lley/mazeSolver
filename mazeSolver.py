fe093316534ff67d2fc7fd10ae3978d1f3918928
0lley <39053286+0lley@users.noreply.github.com>
2020-03-03 20:34:07 +1100

    Input stuff
    Issue with it only putting the first line into the 2d list, MUST FIX

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,18 +1,28 @@
-fle = open("mazeInput.txt", "r")
-maze = [[x for x in range()] for x in range(m)] 
+inpFle = 'mazeInput.txt'
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-#https://stackoverflow.com/questions/845058/how-to-get-line-count-of-a-large-file-cheaply-in-python
+def fileLength(fname):
+    i = 0
+    for line in open(fname):
+        i += 1
+    return(i)
 
-for i in range(5):
-    maze.append(fle.readline())
+with open(inpFle, 'r') as file:
+    data = file.read().replace(', ', '')
+    open(inpFle, "w").write(data)
+#https://stackoverflow.com/questions/19201575/python-read-file-look-up-a-string-and-remove-characters
 
-
+maze = [[x for x in open(inpFle, "r").readline()] for y in range(fileLength(inpFle))] 
+#Setting up the actual maze file
 
 for i in range(len(maze)):
-    for j in range(len(maze[i])):
-        print(j)
+    del maze[i][len(maze[i])-1]
+#Removes line breaks from the 2d list
+
+print(maze)
+
+# for i in range(5):
+#     maze.append(fle.readline())
+
+# for i in range(len(maze)):
+#     for j in range(len(maze[i])):
+#         print(j)
