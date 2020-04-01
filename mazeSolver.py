43e608ffb704dca8955765448605944cdf350e97
0lley <39053286+0lley@users.noreply.github.com>
2020-04-01 21:26:35 +1100

    IGNORE EVERYTHING BEFORE THIS

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,41 +1,21 @@
-fle = open("mazeInput.txt", "r")
-maze = [[x for x in range()] for x in range(m)] 
-inpFle = 'mazeInput.txt'
+inpFile = 'mazeInput.txt'
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-#https://stackoverflow.com/questions/845058/how-to-get-line-count-of-a-large-file-cheaply-in-python
-
-for i in range(5):
-    maze.append(fle.readline())
 def fileLength(fname):
     i = 0
     for line in open(fname):
         i += 1
     return(i)
 
-with open(inpFle, 'r') as file:
-    data = file.read().replace(', ', '')
-    open(inpFle, "w").write(data)
-#https://stackoverflow.com/questions/19201575/python-read-file-look-up-a-string-and-remove-characters
+###################################
+###################################
+###################################
 
-maze = [[x for x in open(inpFle, "r").readline()] for y in range(fileLength(inpFle))] 
-#Setting up the actual maze file
+with open(inpFile, 'r') as file:
+    data = file.read().replace(', ', ' ')
+    open(inpFile, "w").write(data)
 
-for i in range(len(maze)):
-    for j in range(len(maze[i])):
-        print(j)
-    del maze[i][len(maze[i])-1]
-#Removes line breaks from the 2d list
+with open("mazeInput.txt", "r") as file:
+    maze = [[int(i) for i in line.split()] for line in file]
+#https://stackoverflow.com/questions/19201575/python-read-file-look-up-a-string-and-remove-characters user Bartosz Marcinkowski
 
 print(maze)
-
-# for i in range(5):
-#     maze.append(fle.readline())
-
-# for i in range(len(maze)):
-#     for j in range(len(maze[i])):
-#         print(j)
