f0e262e27b556c02f2b2f9dda23dc33073071266
0lley <39053286+0lley@users.noreply.github.com>
2020-04-01 20:32:28 +1100

    Fricked some things up

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,28 +1,38 @@
-inpFle = 'mazeInput.txt'
-
-def fileLength(fname):
+def fileHeight(fname):
     i = 0
     for line in open(fname):
         i += 1
     return(i)
+    #Returns amount of rows in maze
+
+def fileWidth(fname):
+    i = 0
+    for char in open(fname).readline():
+        i += 1
+    return(i)
+# GOTTA WORK FOR THE ACTUAL THINGY
+
+###################################
+###################################
+##################################
+
+inputFile = 'mazeInput.txt'
 
 with open(inpFle, 'r') as file:
     data = file.read().replace(', ', '')
     open(inpFle, "w").write(data)
 #https://stackoverflow.com/questions/19201575/python-read-file-look-up-a-string-and-remove-characters
 
-maze = [[x for x in open(inpFle, "r").readline()] for y in range(fileLength(inpFle))] 
-#Setting up the actual maze file
+maze = [[x for x in open(inputFile, "r").readline()] for y in range(fileWidth(inputFile))] 
 
-for i in range(len(maze)):
-    del maze[i][len(maze[i])-1]
-#Removes line breaks from the 2d list
+# for row in range(fileHeight('mazeInput.txt')):
+#     for column in range(fileWidth('mazeInput.txt')):
+#         maze[row][column] = '0'
+
+# with open("mazeInput.txt", 'r') as f:
+#     for line in f:
+#         lineLis = line.split(',')
+#         for char in line.strip():
+#             maze[]
 
 print(maze)
-
-# for i in range(5):
-#     maze.append(fle.readline())
-
-# for i in range(len(maze)):
-#     for j in range(len(maze[i])):
-#         print(j)
