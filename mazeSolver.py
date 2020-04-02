571d08dc011e8a4cc99d83785a224509dc6a30e8
0lley <39053286+0lley@users.noreply.github.com>
2020-04-02 19:39:41 +1100

    OK THIS IS A MISTAKE

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,32 +1,42 @@
 inpFile = 'mazeInput.txt'
-stack = []
+Moves = []
+Queue = []
 adjSquares = {}
 class completedPath(Exception): pass
 
-def possMoves(y, x):
+def possMoves():
     adjSquares.clear()
-    if y > 0 and maze[y - 1][x] != 1 and maze[y - 1][x] != 5:
-        adjSquares[str(y - 1) + str(x)] = maze[y - 1][x]
-    if y < len(maze) - 1 and maze[y + 1][x] != 1 and maze[y + 1][x] != 5:
-        adjSquares[str(y + 1) + str(x)] = maze[y + 1][x]
-    if x > 0 and maze[y][x - 1] != 1 and maze[y][x - 1] != 5:
-        adjSquares[str(y) + str(x - 1)] = maze[y][x - 1]
-    if x < len(maze[y]) - 1 and maze[y][x + 1] != 1 and maze[y][x + 1] != 5:
-        adjSquares[str(y) + str(x + 1)] = maze[y][x + 1]
+        if y > 0 and maze[y - 1][x] != 1 and maze[y - 1][x] != 5:
+            adjSquares[str(y - 1) + str(x)] = maze[y - 1][x]
+        if y < len(maze) - 1 and maze[y + 1][x] != 1 and maze[y + 1][x] != 5:
+            adjSquares[str(y + 1) + str(x)] = maze[y + 1][x]
+        if x > 0 and maze[y][x - 1] != 1 and maze[y][x - 1] != 5:
+            adjSquares[str(y) + str(x - 1)] = maze[y][x - 1]
+        if x < len(maze[y]) - 1 and maze[y][x + 1] != 1 and maze[y][x + 1] != 5:
+            adjSquares[str(y) + str(x + 1)] = maze[y][x + 1]
+        for j in adjSquares:
+            Moves.append(Moves[j].append(adjSquares))
+        del Moves[i]
+        
+        # else:
 
-def move():
-    possMoves(int(stack[-1][0]), int(stack[-1][1]))
-    if len(adjSquares) == 0:
-        del stack[-1]
-    if 3 in adjSquares.values():
-        print('DONE')
-        raise completedPath
-    else:
-        for i in adjSquares:
-            if adjSquares[i] == 0:
-                stack.append(i) #
-                maze[int(stack[-1][0])][int(stack[-1][1])] = 5
-                break
+
+# def move():
+#     for i in range(len(Moves)):
+#         possMoves()
+
+    # possMoves(int(stack[0][0]), int(stack[-1][1]))
+    # if len(adjSquares) == 0:
+    #     del stack[-1]
+    # if 3 in adjSquares.values():
+    #     print('DONE')
+    #     raise completedPath
+    # else:
+    #     for i in adjSquares:
+    #         if adjSquares[i] == 0:
+    #             stack.append(i) #
+    #             maze[int(stack[-1][0])][int(stack[-1][1])] = 5
+    #             break
 
 with open(inpFile, 'r') as file:
     data = file.read().replace(', ', ' ')
@@ -39,14 +49,16 @@ with open("mazeInput.txt", "r") as file:
   
 for row, column in enumerate(maze):
     if 5 in column:
-        stack.append(str(row) + str(column.index(5)))
+        Moves.append(str(row) + str(column.index(5)))
 #https://stackoverflow.com/questions/5775352/python-return-2-ints-for-index-in-2d-lists-given-item
 
-try:
-    while True:
-        move()
-        print(adjSquares)
-except completedPath:
-    pass
+possMoves()
 
-print(stack)
+# try:
+#     while True:
+#         move()
+#         print(adjSquares)
+# except completedPath:
+#     pass
+
+print(Moves)
