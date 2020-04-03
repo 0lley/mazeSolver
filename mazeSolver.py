a74c709c68ca29d1ea04da0d8f9da2dbb4c6843a
0lley <39053286+0lley@users.noreply.github.com>
2020-04-03 16:16:22 +1100

    Actual final

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,52 +1,63 @@
-inpFile = 'mazeInput.txt'
-stack = []
+inpFile = 'test_maze1.txt'
 adjSquares = {}
-class completedPath(Exception): pass
+Queue = []
+PassMoves = []
+class completedPath(Exception): pass #Allows for the program to end when the exit is reached
 
-def possMoves(y, x):
+def possMoves(y, x, notAllowed):
     adjSquares.clear()
-    if y > 0 and maze[y - 1][x] != 1 or 5:
+    if y > 0 and str(maze[y - 1][x]) not in notAllowed: #Checks if the tile above is traversible
         adjSquares[str(y - 1) + str(x)] = maze[y - 1][x]
-    elif y < len(maze) - 1 and maze[y + 1][x] != 1 or 5:
+        
+    if y < len(maze) - 1 and str(maze[y + 1][x]) not in notAllowed: #Checks if the tile below is traversible
         adjSquares[str(y + 1) + str(x)] = maze[y + 1][x]
-    elif x > 0 and maze[y][x - 1] != 1 or 5:
+
+    if x > 0 and str(maze[y][x - 1]) not in notAllowed: #Checks if the tile 
         adjSquares[str(y) + str(x - 1)] = maze[y][x - 1]
-    elif x < len(maze[0]) and maze[y][x + 1] != 1 or 5:
+
+    if x < len(maze[0]) - 1 and str(maze[y][x + 1]) not in notAllowed:
+        #if str(maze[y][x + 1]) not in Queue[0].strip():
         adjSquares[str(y) + str(x + 1)] = maze[y][x + 1]
 
-def move():
-    possMoves(int(stack[-1][0]), int(stack[-1][1]))
+def move(): ###FIX THIS UP SO IT GOES FROM 3 TO 5
+    possMoves(int(Queue[0][0]), int(Queue[0][1]), '12')
+
     if len(adjSquares) == 0:
-        del stack[-1]
-    if 3 in adjSquares.values():
+        Queue.pop(0)
+
+    elif 3 in adjSquares.values():
         print('DONE')
+        PassMoves.append(Queue.pop(0))
         raise completedPath
+
     else:
         for i in adjSquares:
-            if adjSquares[i] == 0:
-                stack.append(str(i))
-                maze[int(stack[-1][0])][int(stack[-1][1])] = 5
-                break
+            if i in Queue[0].strip():
+                continue
+            else:
+                Queue.append(i + ' ' + Queue[0])
+        Queue.pop(0)
 
 with open(inpFile, 'r') as file:
     data = file.read().replace(', ', ' ')
     open(inpFile, "w").write(data)
 #https://stackoverflow.com/questions/19201575/python-read-file-look-up-a-string-and-remove-characters
 
-with open("mazeInput.txt", "r") as file:
+with open(inpFile, "r") as file:
     maze = [[int(i) for i in line.split()] for line in file]
 #https://stackoverflow.com/questions/19201575/python-read-file-look-up-a-string-and-remove-characters user Bartosz Marcinkowski
   
 for row, column in enumerate(maze):
     if 5 in column:
-        stack.append(str(row) + str(column.index(5)))
+        Queue.append(str(row) + str(column.index(5)))
 #https://stackoverflow.com/questions/5775352/python-return-2-ints-for-index-in-2d-lists-given-item
 
 try:
     while True:
         move()
-        print(adjSquares)
 except completedPath:
     pass
 
-print(maze)
+print(PassMoves)
+
+#(PassMoves)
